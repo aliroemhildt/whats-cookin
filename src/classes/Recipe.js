9f4935d1d6d08708b665fe39994bf89c3f0c9e1c
Cesare Gallo <cesaregallo0@gmail.com>
2021-12-08 14:49:48 -0700

    Begin recipe class

---
+++ src/classes/Recipe.js
@@ -0,0 +1,17 @@
+class Recipe {
+    constructor() {
+
+    }
+}
+
+// A Recipe represents one recipe object (name, id, ingredients, instructions).
+// pass in recipe data 
+
+// It should hold on to all its information (provided in the data file).
+// It should have methods to:
+// Determine the names of ingredients needed
+// Get the cost of its ingredients
+// Return its directions / instructions
+
+export default Recipe
+
