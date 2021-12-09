1179b86ef55a1761e4563758d7cb5dbfcfa435e5
Cesare Gallo <cesaregallo0@gmail.com>
2021-12-09 14:13:59 -0700

    Fix recipe repo tests with sample data

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -2,345 +2,12 @@ import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
 import recipeData from '../src/data/recipes';
 import ingredientsData from '../src/data/ingredients';
-//import sampleData from '..src/data/sampleData';
+import sampleData from '../src/data/sampleData';
 
 describe('RecipeRepository', () => {
-  let sampleData;
-  //let recipeData
   let repository;
 
   beforeEach(() => {
-    sampleData = [
-      {
-        "id": 595736,
-        "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
-        "ingredients": [
-          {
-            "id": 20081,
-            "quantity": {
-              "amount": 1.5,
-              "unit": "c"
-            }
-          },
-          {
-            "id": 18372,
-            "quantity": {
-              "amount": 0.5,
-              "unit": "tsp"
-            }
-          },
-          {
-            "id": 1123,
-            "quantity": {
-              "amount": 1,
-              "unit": "large"
-            }
-          },
-          {
-            "id": 19335,
-            "quantity": {
-              "amount": 0.5,
-              "unit": "c"
-            }
-          },
-          {
-            "id": 19206,
-            "quantity": {
-              "amount": 3,
-              "unit": "Tbsp"
-            }
-          },
-          {
-            "id": 19334,
-            "quantity": {
-              "amount": 0.5,
-              "unit": "c"
-            }
-          },
-          {
-            "id": 2047,
-            "quantity": {
-              "amount": 0.5,
-              "unit": "tsp"
-            }
-          },
-          {
-            "id": 1012047,
-            "quantity": {
-              "amount": 24,
-              "unit": "servings"
-            }
-          },
-          {
-            "id": 10019903,
-            "quantity": {
-              "amount": 2,
-              "unit": "c"
-            }
-          },
-          {
-            "id": 1145,
-            "quantity": {
-              "amount": 0.5,
-              "unit": "c"
-            }
-          },
-          {
-            "id": 2050,
-            "quantity": {
-              "amount": 0.5,
-              "unit": "tsp"
-            }
-          }
-        ],
-        "instructions": [
-          {
-            "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
-            "number": 1
-          },
-          {
-            "instruction": "Add egg and vanilla and mix until combined.",
-            "number": 2
-          },
-          {
-            "instruction": "Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.",
-            "number": 3
-          },
-          {
-            "instruction": "Place the cookie dough balls into ungreased muffin pan. Sprinkle with sea salt.",
-            "number": 4
-          },
-          {
-            "instruction": "Bake for 9 to 10 minutes, or until you see the edges start to brown.",
-            "number": 5
-          },
-          {
-            "instruction": "Remove the pan from the oven and let sit for 10 minutes before removing onto a cooling rack.Top with ice cream and a drizzle of chocolate sauce.",
-            "number": 6
-          }
-        ],
-        "name": "Loaded Chocolate Chip Pudding Cookie Cups",
-        "tags": [
-          "antipasti",
-          "starter",
-          "snack",
-          "appetizer",
-          "antipasto",
-          "hor d'oeuvre"
-        ]
-      },
-      {
-        "id": 678353,
-        "image": "https://spoonacular.com/recipeImages/678353-556x370.jpg",
-        "ingredients": [
-          {
-            "id": 1009016,
-            "quantity": {
-              "amount": 1.5,
-              "unit": "cups"
-            }
-          },
-
-          {
-            "id": 9003,
-            "quantity": {
-              "amount": 2,
-              "unit": ""
-            }
-          },
-          {
-            "id": 20027,
-            "quantity": {
-              "amount": 1,
-              "unit": "tablespoon"
-            }
-          },
-          {
-            "id": 1002046,
-            "quantity": {
-              "amount": 1,
-              "unit": "tablespoon"
-            }
-          },
-          {
-            "id": 11215,
-            "quantity": {
-              "amount": 1,
-              "unit": "clove"
-            }
-          },
-          {
-            "id": 1012046,
-            "quantity": {
-              "amount": 1,
-              "unit": "tablespoon"
-            }
-          },
-          {
-            "id": 19911,
-            "quantity": {
-              "amount": 0.25,
-              "unit": "cup"
-            }
-          },
-          {
-            "id": 16112,
-            "quantity": {
-              "amount": 1,
-              "unit": "tablespoon"
-            }
-          },
-          {
-            "id": 10010062,
-            "quantity": {
-              "amount": 24,
-              "unit": "ounce"
-            }
-          },
-          {
-            "id": 1102047,
-            "quantity": {
-              "amount": 4,
-              "unit": "servings"
-            }
-          },
-          {
-            "id": 16124,
-            "quantity": {
-              "amount": 1,
-              "unit": "tablespoon"
-            }
-          },
-          {
-            "id": 1016168,
-            "quantity": {
-              "amount": 1,
-              "unit": "tablespoon"
-            }
-          }
-        ],
-        "instructions": [
-          {
-            "instruction": "Season the pork chops with salt and pepper and grill or pan fry over medium high heat until cooked, about 3-5 minutes per side. (If grilling, baste the chops in the maple dijon apple cider sauce as you grill.)Meanwhile, mix the remaining ingredients except the apple slices, bring to a simmer and cook until the sauce thickens, about 2-5 minutes.Grill or saute the apple slices until just tender but still crisp.Toss the pork chops and apple slices in the maple dijon apple cider sauce and enjoy!",
-            "number": 1
-          }
-        ],
-        "name": "Maple Dijon Apple Cider Grilled Pork Chops",
-        "tags": [
-          "lunch",
-          "main course",
-          "main dish",
-          "dinner"
-        ]
-      },
-      {
-        "id": 412309,
-        "image": "https://spoonacular.com/recipeImages/412309-556x370.jpeg",
-        "ingredients": [
-          {
-            "id": 1002030,
-            "quantity": {
-              "amount": 4,
-              "unit": "teaspoons"
-            }
-          },
-          {
-            "id": 19334,
-            "quantity": {
-              "amount": 8,
-              "unit": "tablespoons"
-            }
-          },
-          {
-            "id": 1001,
-            "quantity": {
-              "amount": 2,
-              "unit": "cups"
-            }
-          },
-          {
-            "id": 4582,
-            "quantity": {
-              "amount": 4,
-              "unit": "servings"
-            }
-          },
-          {
-            "id": 2031,
-            "quantity": {
-              "amount": 4,
-              "unit": "teaspoons"
-            }
-          },
-          {
-            "id": 5100,
-            "quantity": {
-              "amount": 1,
-              "unit": "pound"
-            }
-          },
-          {
-            "id": 2009,
-            "quantity": {
-              "amount": 4,
-              "unit": "teaspoons"
-            }
-          },
-          {
-            "id": 1022020,
-            "quantity": {
-              "amount": 4,
-              "unit": "teaspoons"
-            }
-          },
-          {
-            "id": 6168,
-            "quantity": {
-              "amount": 8,
-              "unit": "cups"
-            }
-          },
-          {
-            "id": 9176,
-            "quantity": {
-              "amount": 0.5,
-              "unit": "cup"
-            }
-          },
-          {
-            "id": 2026,
-            "quantity": {
-              "amount": 4,
-              "unit": "teaspoons"
-            }
-          },
-          {
-            "id": 1042047,
-            "quantity": {
-              "amount": 1.5,
-              "unit": "tablespoons"
-            }
-          },
-          {
-            "id": 1042047,
-            "quantity": {
-              "amount": 4,
-              "unit": "teaspoons"
-            }
-          }
-        ],
-        "instructions": [
-          {
-            "instruction": "Mix the hot sauce, butter, mango habanero sauce, brown sugar, chili powder, garlic powder, onion powder, black pepper, cayenne pepper and seasoning salt in a bowl. Stir vigorously until completely combined.",
-            "number": 1
-          }
-        ],
-        "name": "Dirty Steve's Original Wing Sauce",
-        "tags": [
-          "sauce"
-        ]
-      }
-    ]
     repository = new RecipeRepository(sampleData);
   });
 
@@ -355,7 +22,6 @@ describe('RecipeRepository', () => {
   it('should take in a parameter of recipe data', () => {
     expect(repository.recipeData).to.equal(sampleData);
   });
-  //we are building out another file with sample data for recipes and another for ingredients (see the following test below)
 
   it('should have a property that lists which recipes to show on the page, ', () => {
     expect(repository).to.have.property('recipesToShow');
@@ -372,10 +38,8 @@ describe('RecipeRepository', () => {
 
   it('should return a list of recipes based on mutliple tags', () => {
     let recipesByTag = repository.filterByTags(['sauce', 'snack']);
-    expect(recipesByTag).to.deep.equal([sampleData[0], sampleData[2]]);
+    expect(recipesByTag).to.deep.equal([sampleData[0], sampleData[2], sampleData[7]]);
   });
-  //what the...this passes the test but the indecies are backwards?
-  // testing
   
   it('should be able to take in an ingredient name and return an array containing that ingredient\s ID', () => {
     const getIngID = repository.getIngredientID('wheat flour', ingredientsData);
@@ -394,14 +58,11 @@ describe('RecipeRepository', () => {
     const recipesByIngredient = repository.filterByNameOrIng('brown sugar', ingredientsData);
     let recipe1 = sampleData[0];
     let recipe2 = sampleData[2];
-    //let recipe 3 = sampleData[6];
-    //let recipe4 = sampleData[8];
-    let recipesTest = [recipe1, recipe2];
-    // let recipesTest = [recipe1, recipe2, recipe3, recipe4];
+    let recipe3 = sampleData[6];
+    let recipe4 = sampleData[8];
+    let recipesTest = [recipe1, recipe2, recipe3, recipe4];
 
     expect(recipesByIngredient).to.deep.equal(recipesTest);
   });
-
-
 
 });
