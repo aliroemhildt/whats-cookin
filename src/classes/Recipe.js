0babdf7d7b09936129e33cbdc6095e38c06058d8
Katie Ammon <katie.ammon10@gmail.com>
2021-12-09 15:06:47 -0700

    Add refactor recipe class

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -13,31 +13,21 @@ class Recipe {
     let ingredientIDs = this.ingredients.reduce((acc, ingredient) => {
       if(!acc.includes(ingredient.id)) {
         acc.push(ingredient.id)
-      }
-      console.log('acc', acc)
-
-      return acc
-    }, [])
-
+      };
+      return acc;
+    }, []);
 
     const matchNamesWithID = ingredientIDs.reduce((acc, currentID) => {
-      //iterate through ingredientIDs
       const ingredientMatch = ingredientsData.find((ingredient) => {
         return ingredient.id === currentID
-      })
-      acc.push(ingredientMatch.name)
-      //match with id in object of ingredientsData
-      // if === return associated name
-    
-      return acc
-    },[])
-    return matchNamesWithID
-    //matching the id no. to the ingredient name in the igredientsData
-    // iterate through the ingredients object and return an array of id numbers.
-    // map the array of id numbers and find the matching id number in the ingredients list and return the associated name of the ingredients in an array
-  }
+      });
+      acc.push(ingredientMatch.name);
+      return acc;
+    }, []);
 
-}
+    return matchNamesWithID;
+  };
+};
 
 // A Recipe represents one recipe object (name, id, ingredients, instructions).
 // pass in recipe data
