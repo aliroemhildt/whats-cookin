292857a58bdb067efaf8b960776a4df4568842e8
Katie Ammon <katie.ammon10@gmail.com>
2021-12-08 17:57:15 -0700

    Add recipe-test

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -5,10 +5,55 @@ import ingredientsData from '../src/data/ingredients';
 
 
 describe('Recipe', () => {
-    let recipe;
+  let recipe;
 
-    beforeEach(() => {
-        recipe = new Recipe();
-    })
+  beforeEach(() => {
+    recipe = new Recipe(recipeData[0]);
+  })
+  
+  it('should be a function', () => {
+    expect(Recipe).to.be.a('function');
+  });
+
+  it('should be an instance of Recipe Repository', () => {
+    expect(recipe).to.be.an.instanceof(Recipe);
+  });
+
+  it('should hold all the info for a recipe', () => {
+    expect(recipe).to.deep.equal(recipeData[0]);
+  })
+
+  it('should have an id', () => {
+      expect(recipe).to.have.property('id').value
+  })
+
+  it('should have an ingredients property', () => {
+    expect(recipe).to.have.property('ingredients').value
+  })
+
+  it('should have an instructions property', () => {
+    expect(recipe).to.have.property('instructions').value
+  })
+
+  it('should have a name property', () => {
+    expect(recipe).to.have.property('name').value
+  })
+
+  it('should have a tags property', () => {
+    expect(recipe).to.have.property('id').value
+  })
+
+  it('should have a method to determine the names of ingredients', () => {
+    expect(recipe.determineIng).to.deep.equal([])
+  }) 
+
+  it('should get the cost of its ingredients', () => {
+    expect(recipe.calculateRecipeCost).to.equal('suprise value!!')
+  })
+
+  it('should return its instructions', () => {
+    expect(recipe.getInstructions).to.equal('stuff...{}?')
+  })
+    
 })
 
