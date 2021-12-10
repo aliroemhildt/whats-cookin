0ea76b871c37d8804832ebcacf734f866ba08f9e
Ali Roemhildt <aliroemhildt@gmail.com>
2021-12-09 20:41:35 -0700

    WIP

--- src/scripts.js
+++ src/scripts.js
@@ -3,19 +3,26 @@ import './styles.css';
 import apiCalls from './apiCalls';
 import ingredientsData from '../src/data/ingredients';
 import recipeData from '../src/data/recipes';
+
 import RecipeRepository from '../src/classes/RecipeRepository';
 import Recipe from '../src/classes/Recipe';
 
 const recipeRepository = new RecipeRepository(recipeData);
 
 // QUERY SELECTORS
-// do we want these to be var/const/let
-
-
-
+const recipeSection = query.selector("recipes-section");
 
 // EVENT LISTENERS
-
+recipeSection.addEventListener('load', displayAllRecipes());
 
 // FUNCTIONS
-console.log('Hello World!');
+const displayAllRecipes = () => {
+  // iterate through recipeRepository:
+  //
+}
+
+// <section class='recipe-card'>
+//   <img src="" alt="" class="recipe-photo">
+//   <img src="" alt="favorite-button" class="favorite-button">
+//   <p>Recipe Name</p>
+// </section>
