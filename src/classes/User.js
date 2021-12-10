82f8699fa29aa608cdd6e2a8c5b63a51ca76f847
Cesare Gallo <cesaregallo0@gmail.com>
2021-12-10 14:09:24 -0700

    Add ,methods to user class

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,9 +1,44 @@
 class User {
-    constructor(user) {
-        this.name = user.name;
-        this.id = user.id;
-        this.pantry = user.pantry;
+  constructor(user) {
+    this.name = user.name;
+    this.id = user.id;
+    this.pantry = user.pantry;
+    this.favorites = [];
+    this.recipesToCook = [];
+  }
+
+  addToFavorites(recipe) {
+    if (!this.favorites.includes(recipe)) {
+      this.favorites.push(recipe);
     }
+  }
+
+  removeFromFavorites(recipe) {
+    const recipeIndex = this.favorites.indexOf(recipe);
+    if (recipeIndex !== -1) {
+      this.favorites.splice(recipeIndex, 1);
+    }
+  }
+
+  addToRecipesToCook(recipe) {
+    if (!this.recipesToCook.includes(recipe)) {
+      this.recipesToCook.push(recipe);
+    }
+  }
+
+  removeFromRecipesToCook(recipe) {
+    const recipeIndex = this.recipesToCook.indexOf(recipe);
+    if (recipeIndex !== -1) {
+      this.recipesToCook.splice(recipeIndex, 1);
+    }
+  }
+
+  filterFavoritesByTag(tag) {
+    return this.favorites.filter(recipe => {
+      return recipe.tags.includes(tag);
+    })
+  }
+
 }
 
 export default User;
