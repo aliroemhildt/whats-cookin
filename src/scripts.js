e01957c3f034adeb1c58e61eb89dab928e40a0ce
Ali Roemhildt <aliroemhildt@gmail.com>
2021-12-09 20:15:13 -0700

    Add css rules for recipe cards

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,20 @@
+// GLOBAL
 import './styles.css';
 import apiCalls from './apiCalls';
+import ingredientsData from '../data/ingredients';
+import recipeData from '../src/data/recipes';
+import RecipeRepository from '../src/classes/RecipeRepository';
+import Recipe from '../src/classes/Recipe';
+
+const recipeRepository = new RecipeRepository(recipeData);
+
+// QUERY SELECTORS
+// do we want these to be var/const/let
 
 
 
 
-console.log('Hello world');
+// EVENT LISTENERS
+
+
+// FUNCTIONS
