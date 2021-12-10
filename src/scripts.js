b780b121d74a50c4e5b4fd8d9086ca86b566f022
Alexis Newby <alexis.newby@gmail.com>
2021-12-10 16:03:53 -0700

    Begin to build out the displaySelectedRecipe method in scripts file

--- src/scripts.js
+++ src/scripts.js
@@ -9,11 +9,12 @@ import Recipe from '../src/classes/Recipe';
 
 const recipeRepository = new RecipeRepository(recipeData);
 
-
-//
-
 // QUERY SELECTORS
 const recipeSection = document.querySelector('.recipes-section');
+const selectedRecipeSection = document.querySelector('.individual-recipe-container');
+const filterBar = document.querySelector('.filter-section');
+
+
 let recipeCards
 
 
@@ -21,7 +22,9 @@ let recipeCards
 window.addEventListener('load', displayAllRecipes);
 
 recipeCards.forEach((card) => {
-  card.addEventListener('click', displaySelectedRecipe)
+  card.addEventListener('click', function(e) {
+    displaySelectedRecipe(e)
+  });
 });
 
 
@@ -29,7 +32,7 @@ recipeCards.forEach((card) => {
 function displayRecipes() {
   recipeSection.innerHTML = '';  recipeRepository.recipesToShow.forEach(recipe => {
     recipeSection.innerHTML += `
-      <section class='recipe-card'>
+      <section class='recipe-card' id='id${recipe.id}'>
          <img class='recipe-card-image' src=${recipe.image} alt='recipe image' class='recipe-photo'>
          <button class='favorite-button'>FAVORITE</button>
          <p class='recipe-card-name'>${recipe.name}</p>
@@ -40,16 +43,48 @@ function displayRecipes() {
   recipeCards.forEach((card) => {
     card.addEventListener('click', displaySelectedRecipe)
   });
-};
+}
 
 function displayAllRecipes() {
   recipeRepository.addAllRecipesToRecipesToShow();
   displayRecipes();
-};
+}
 
-function displaySelectedRecipe() {
-  console.log('hey there')
-};
+function displaySelectedRecipe(e) {
+  if(e.target.classList.value !== 'favorite-button') {
+
+    const image = document.querySelector('.selected-recipe-photo-js');
+    const name = document.querySelector('.selected-recipe-name-js');
+    const ingredientList = document.querySelector('.selected-recipe-ingredient-list-js');
+    const cost = document.querySelector('.selected-recipe-cost-js');
+    const instructions = document.querySelector('.selected-recipe-instructions-js');
+
+    const recipeID = Number(e.target.parentNode.id.slice(2));
+    show([selectedRecipeSection]);
+    hide([recipeSection, filterBar]);
+
+    let selectedRecipe = recipeRepository.recipeData.find((currentRecipe) => {
+      return currentRecipe.id === recipeID
+    })
+
+    image.src = selectedRecipe.image;
+    name.innerText = selectedRecipe.name;
+    ingredientList.innerText = selectedRecipe.
+  }
+}
+
+function show(elements) {
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.remove('hidden');
+  }
+}
+
+function hide(elements) {
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.add('hidden');
+  }
+}
+
 
 //need a funtion that will display ONLY the recipe that's clicked on
 //1. click a recipe CARD
