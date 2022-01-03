74b241c03102ab54890a0858e416bef682082177
Katie Ammon <katie.ammon10@gmail.com>
2022-01-03 16:50:51 -0700

    Add pantry test

---
+++ test/Pantry-test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import User from '../src/classes/User';
+import sampleData from '../src/data/sampleData';
+import ingredientsData from '../src/data/ingredients';
+import usersData from '../src/data/users';
+
+describe('Pantry', () => {
+  
+})
